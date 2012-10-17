08f0d96e27ac172fc813f17bf51d321334bdaa67
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-10-17 17:24:21 -0300

    Testing...

--- src/test/Test.java
+++ src/test/Test.java
@@ -7,5 +7,13 @@ public class Test {
 	public void main(String args[])
 	{
 		HashTable hash = new HashTable();
+		System.out.println("HashTable created!");
+		
+		hash.add("Anderson", new int[] {1,2,3});
+		hash.add("Joao", new int[] {2,3,4});
+		
+		hash.printElements();
+		
+		System.out.println("Bye!");
 	}
 }
